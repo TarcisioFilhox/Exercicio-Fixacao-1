975b11a5a580f40ecbb3764715fe7e33f2debaf6
Tarcísio de Pinho Silva Filho <89551990+TarcisioFilhox@users.noreply.github.com>
2021-10-15 21:50:26 -0300

    Create Quest2_endereco.java

---
+++ Quest2_endereco.java
@@ -0,0 +1,17 @@
+package Quest2;
+
+public class Quest2_endereco {
+
+	    String rua, complemento, bairro, cidade, uf, numero, cep;
+
+	    public Quest2_endereco(String numero, String cep, String rua, String complemento, String bairro, String cidade, String uf){
+
+	        this.numero = numero;
+	        this.cep = cep;
+	        this.rua = rua;
+	        this.complemento = complemento;
+	        this.bairro = bairro;
+	        this.cidade = cidade;
+	        this.uf = uf;
+	    }
+	}
