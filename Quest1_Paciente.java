ba1e3a906f201e99a7d23f247b5f0af316ab9a57
Tarcísio de Pinho Silva Filho <89551990+TarcisioFilhox@users.noreply.github.com>
2021-10-15 21:25:55 -0300

    Create Quest1_Paciente.java

---
+++ Quest1_Paciente.java
@@ -0,0 +1,19 @@
+package Quest1;
+
+public class Quest1_paciente {
+
+
+int codigo; String nome, dataNascimento, sexo, planoSaude, alergia, tipoSanguineo;
+
+public Quest1_paciente(int cod, String nome, String data, String sexo, String planoSaude, String alergia, String tipo){
+
+    this.codigo = cod;
+    this.nome = nome;
+    this.dataNascimento = data;
+    this.sexo = sexo;
+    this.planoSaude = planoSaude;
+    this.alergia = alergia;
+    this.tipoSanguineo = tipo;
+
+}
+}
